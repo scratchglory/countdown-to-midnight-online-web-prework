f7faf7b2e377e881301be2646dfc1b0448f77801
Adella <adellakenn@gmail.com>
2019-04-26 13:19:42 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
-def countdown
+# def countdown(number)
+#     number = 1
+#     while number < 10  
+#     puts "#{number} SECOND(S)!"  
+#     number += 1  
+# end
+#   puts "HAPPY NEW YEAR!"
+# end
+
+def countdown(number)
+  number = 10
+  while number > 0 
+  puts "#{number} SECOND(S)!"
+  number -= 1 
+end
+"HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds)
+  sleep(seconds)
 end

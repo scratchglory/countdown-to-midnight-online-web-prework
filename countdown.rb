0c84f6efa1ab7572397f1380a3f042a3622e78ad
Adella <adellakenn@gmail.com>
2019-04-26 14:18:39 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -15,9 +15,16 @@ def countdown(number)
   puts "#{number} SECOND(S)!"
   number -= 1 
 end
+<<<<<<< HEAD
 "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(seconds)
   sleep(seconds)
+=======
+end
+
+def countdown_with_sleep
+  countdown.sleep(5)
+>>>>>>> d3e12c5394763ee770b5195a3363e6e4b3b073ee
 end
